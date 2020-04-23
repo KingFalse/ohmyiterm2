974b3976e9be03cf058b30c465ffdbb188c6f275
鹞之神乐 <yzsl@live.com>
2020-04-23 18:57:33 +0800

    update install script

--- install.sh
+++ install.sh
@@ -120,6 +120,8 @@ echo "正在安装iTerm2-Utilities扩展..."
 sudo unzip -o -q ~/ohmyiterm2/utilities.zip -d ~/.iterm2
 chmod +x ~/.iterm2/*
 echo "alias imgcat=\${HOME}/.iterm2/imgcat;alias imgls=\${HOME}/.iterm2/imgls;alias it2api=\${HOME}/.iterm2/it2api;alias it2attention=\${HOME}/.iterm2/it2attention;alias it2check=\${HOME}/.iterm2/it2check;alias it2copy=\${HOME}/.iterm2/it2copy;alias it2dl=\${HOME}/.iterm2/it2dl;alias it2getvar=\${HOME}/.iterm2/it2getvar;alias it2git=\${HOME}/.iterm2/it2git;alias it2setcolor=\${HOME}/.iterm2/it2setcolor;alias it2setkeylabel=\${HOME}/.iterm2/it2setkeylabel;alias it2ul=\${HOME}/.iterm2/it2ul;alias it2universion=\${HOME}/.iterm2/it2universion" >~/.iterm2_shell_integration.zsh
+
+cp ~/ohmyiterm2/.iterm2_shell_integration.zsh ~/
 chmod +x ~/.iterm2_shell_integration.zsh
 
 # 检测CommandLineTools是否已经安装
